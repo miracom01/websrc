1549c4e3083f4b3e3f58d2db0700e312604f1f53
miracom01 <ronaldolee72@gmail.com>
2016-10-23 01:01:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,40 +2,23 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
-<<<<<<< HEAD
-var fs = require("fs")
-=======
 var fs = require("fs");
->>>>>>> 815a1994b881b84325e123e295609706a6751e5e
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-<<<<<<< HEAD
-var server = app.listen(7777, function(){
- console.log("Group_2 Express server has started on port 7777")
-});
-
-app.use(express.static('public'));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-=======
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
->>>>>>> 815a1994b881b84325e123e295609706a6751e5e
 app.use(session({
  secret: 'miracomSecretKey',
  resave: false,
  saveUninitialized: true
 }));
 
-<<<<<<< HEAD
-=======
 var auth = require('./router/auth')(app);
 var members = require('./router/members')(app);
 var ap_ep = require('./router/ap_ep')(app);
@@ -49,6 +32,5 @@ app.use('/sapi',sapi); //서비스 api
 var server = app.listen(7777, function(){
  console.log("Group_2 Express server has started on port 7777");
 });
->>>>>>> 815a1994b881b84325e123e295609706a6751e5e
 
 var router = require('./router/main')(app, fs);
