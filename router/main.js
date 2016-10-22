b5205097d3c14b27b18db1337f84ebd82171ce62
Lee Youngmin <ronaldolee72@gmail.com>
2016-10-23 01:40:40 +0900

    main.js수정

--- router/main.js
+++ router/main.js
@@ -4,12 +4,6 @@ module.exports = function(app, fs)
          res.render('index', {
              title: "MY HOMEPAGE",
              length: 5
-<<<<<<< HEAD
          })
      });
 }
-=======
-         });
-     });
-};
->>>>>>> 815a1994b881b84325e123e295609706a6751e5e
