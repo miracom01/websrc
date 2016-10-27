1ca5c59d9c84d480ff5f7235cfed4cbc30f8906f
miracom01 <ronaldolee72@gmail.com>
2016-10-28 01:59:51 +0900

    routing test3
    routing test3

--- router/ap_ep.js
+++ router/ap_ep.js
@@ -2,9 +2,13 @@
 module.exports = function(app) {
   var route = require('express').Router();
 
-  route.get('/apepmanage',function(req,res){
+  route.get('/ApEpManagement',function(req,res){
       res.render('ApEpManagement', {});
 
+  });
+
+  route.post('/ApEpManagement', function(req, res){
+    res.render('ApEpManagement', {});
   });
 
   route.get('/listApEpList',function(req,res) {
