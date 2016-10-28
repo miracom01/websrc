4efe0dae02b53fe9c107c8794cdf73f422fc2183
miracom01 <ronaldolee72@gmail.com>
2016-10-28 13:48:17 +0900

    comit test
    commit test

--- router/main.js
+++ router/main.js
@@ -1,5 +1,40 @@
 module.exports = function(app, fs)
 {
-     
+
+
+
+  route.post('/main', function(req, res, next){
+    //txtInputApSN
+    //txtInputApName
+    res.send('user id: ' + req.session.user_id + ' /ap_name: ' +req.body.addApform.txtInputApName+ ' /ap_sn' +req.body.addApform.txtInputApSN );
+
+
+      console.log(req.session.user_id, req.body.addApform.txtInputApName, req.body.addApform.txtInputApSN);
+      var user = {
+        user_id:req.session.user_id,
+        ap_name:req.body.addApform.txtInputApName,
+        ap_sn:req.body.addApform.txtInputApSN
+      };
+
+      res.send('user id: ' + req.session.user_id + ' /ap_name: ' +req.body.txtInputApName+ ' /ap_sn' +req.body.txtInputApSN );
+
+      /*
+      var sql = 'INSERT INTO TB_EQUIP_MASTER SET ?';
+      conn.query(sql, user, function(err, results){
+        if(err){
+          console.log(err);
+          res.status(500);
+        } else {
+          req.session.user_id = user.user_id;
+          req.session.user_name = user.user_name;
+          console.log("login finished : "+req.session.user_id);
+          req.session.save(function(){
+            res.redirect('/main');
+            //res.render('index',{sessionId:req.session.user_id});
+          });
+        }
+      });
+      */
+  });
 
 };
