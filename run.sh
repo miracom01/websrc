74da232c6652acfb3b3e6c710cfbd2e0c267460b
Lee YoungMin <ronaldolee72@gmail.com>
2016-10-31 12:38:51 +0000

    modify sh파일

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #echo "[`date +%Y/%m/%d_%H:%M:%S`]" >> ./log/log.out
 #nohup node ./server.js >> ./log/log.out &
+cd /home/ubuntu/group_2
 
 PROC_NAME="server.js"
-PID=`ps -ef | grep "${PROC_NAME}" | grep -v "grep" | cut -d" " -f4`
+PID=`ps -ef | grep "${PROC_NAME}" | grep -v "grep" | awk '{print $2}'  `
 
 if [ ${PID} ];then
   echo "[`date +%Y/%m/%d_%H:%M:%S`] Server still alive::::::PID : ${PID}"
 else
  # echo "${PROC_NAME} KILL PID : ${PID}"
  # kill ${PID}
-  nohup node /home/ubuntu/group_2/${PROC_NAME} &
+  nohup node ${PROC_NAME} &
   echo "[`date +%Y/%m/%d_%H:%M:%S`] restart Server!"
 fi
+
